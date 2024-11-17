58015e91fdf2c0a24a1ed0558f98df24e572a0f8
JUNED <juned9296@gmail.com>
2024-11-17 14:39:34 +0530

    [Everything is updated]

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from 'path';
+
 
 
 import bookRoute from "./route/book.route.js";
@@ -33,14 +33,14 @@ try {
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
-//  deployment
-if(process.env.NODE_ENV === "production"){
-    const dirPath = path.resolve();
-    app.use(express.static("Frontend/dist"));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(dirPath,"./Frontend/dist","index.html"));
-    })
-  }
+// //  deployment
+// if(process.env.NODE_ENV === "production"){
+//     const dirPath = path.resolve();
+//     app.use(express.static("Frontend/dist"));
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.resolve(dirPath,"./Frontend/dist","index.html"));
+//     })
+//   }
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
